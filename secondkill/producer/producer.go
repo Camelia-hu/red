1d95a5e5dd33a0a89bdf2ec782b4b661469b6002
Camelia-hu <160568871+Camelia-hu@users.noreply.github.com>
2024-05-17 11:51:30 +0800

    Update producer.go

--- secondkill/producer/producer.go
+++ secondkill/producer/producer.go
@@ -8,27 +8,27 @@ import (
 )
 
 func ProduceKafka() {
-	// 配置 kafka 生产者
+	
 	config := sarama.NewConfig()
 	config.Producer.RequiredAcks = sarama.WaitForAll
 	config.Producer.Retry.Max = 5
 	config.Producer.Return.Successes = true
 
-	// 创建 Kafka 生产者
+	
 	producer, err := sarama.NewSyncProducer([]string{"192.168.89.184:9092"}, config)
 	if err != nil {
 		log.Fatalf("Creating producer: %v", err)
 	}
-	// 延迟关闭生产者链接
+	
 	defer producer.Close()
 
-	// 定义消息 Topic是 go-test, 值为 Hello Kafka
+	
 	message := &sarama.ProducerMessage{
 		Topic: "killers",
 		Value: sarama.StringEncoder("kill success!"),
 	}
 
-	// 发送消息
+	
 	for i := 0; i < 10; i++ {
 		partition, offset, err := producer.SendMessage(message)
 		if err != nil {
